a910fdc023127ff8846857f314c55cd1d860fb26
mi0ski & zAHAHAHAHAHAHAr <137182775+k0sen4n47@users.noreply.github.com>
2025-02-23 22:12:00 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+function isEven() {
+  const date = new Date();
+  const currentDay = date.getDate();
+  return currentDay % 2 === 0;
+}
+console.log(`Сегодня ${isEven() ? 'чётный' : 'нечётный'} день.`);
+module.exports = { isEven };
