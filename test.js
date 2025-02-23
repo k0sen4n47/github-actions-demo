3e2443554dba6d1f28bcbb2c43c2fe2b21c0da37
mi0ski & zAHAHAHAHAHAHAr <137182775+k0sen4n47@users.noreply.github.com>
2025-02-23 22:13:12 +0300

    test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+const { isEven } = require('./index.js');
+describe('isEven', function() {
+  it('should return true if the current day is even', function() {
+    const date = new Date();
+    const currentDay = date.getDate();
+    const expected = currentDay % 2 === 0;
+    assert.strictEqual(isEven(), expected);
+  });
+});
